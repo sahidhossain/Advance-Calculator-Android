924bdb17895f7b04dee841ea7a916f6f635545c1
sahid hossain <mohammadsahid786cse@gmail.com>
2018-11-03 15:41:32 +0600

    first commit

---
+++ app/src/main/java/com/example/sahid/advancecalculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.example.sahid.advancecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
